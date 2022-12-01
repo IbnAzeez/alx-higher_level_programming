0161692434c86af81336e2103bf59cc14d3c42de
IbnAzeez <hayzed003@gmail.com>
2022-12-01 14:18:02 -0800

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
         print("{:d} argument".format(num - 1), end='')
     else:
         print("{:d} arguments".format(num - 1), end='')
-        if (num - 1 == 0):
-            print('.')
-        else:
-            print(':')
-            for i in range(1, num):
-                print("{}: {}".format(i, sys.argv[i]))
+    if (num - 1 == 0):
+        print('.')
+    else:
+        print(':')
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
