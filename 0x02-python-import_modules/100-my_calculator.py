d15801fd2cde6f22b1b84313ca5d1b94fe536df7
IbnAzeez <hayzed003@gmail.com>
2022-12-06 05:25:07 -0800

    Easy print

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,9 +2,10 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+    def calc(argv):
+        if len(sys.argv) - 1 != 3:
+            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+            sys.exit(1)
     a = int(sys.argv[1]) 
     b = int(sys.argv[3])
     operator = sys.argv[2]
