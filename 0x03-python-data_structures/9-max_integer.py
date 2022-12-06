2c55970747e7c401a7198fd0f84128a59cb81401
IbnAzeez <hayzed003@gmail.com>
2022-12-06 02:44:24 -0800

    Max Integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,9 @@
 def max_integer(my_list=[]):
     if 0 == len(my_list):
         return None
-    return sorted(my_list)[-1]
+    number = min(my_list)
+    for num in my_list:
+        if num > number:
+            number = num
+    return number
 
