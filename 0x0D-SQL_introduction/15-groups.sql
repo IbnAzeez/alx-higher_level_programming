2c6d8245fe1c0bbf8dff7f4627da87274dd9c9e9
IbnAzeez <hayzed003@gmail.com>
2023-02-20 05:30:19 -0800

    Num by Score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,6 +2,6 @@
 -- db hbtn_0c_0: result displays the score, the number of records for this score
 -- with the label number, sorted by number of records in descending order.
 
-SELECT `score`, COUNT(*) AS `number`
+SELECT `score`, COUNT(score) AS `number`
 FROM second_table
 GROUP by `score` DESC;
