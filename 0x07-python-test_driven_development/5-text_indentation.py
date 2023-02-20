1d2a852bfbadd732326bdeb55c4cbc9277ae7952
IbnAzeez <hayzed003@gmail.com>
2023-02-20 03:40:31 -0800

    Indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" text_indentation module """
+
+
+def text_indentation(text):
+    """ text_indentation function
+    this function split a text by punctuation
+    Attributes:
+        prmText: text to split
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    text = text.strip()
+    text = text.replace(".", ".\n")
+    text = text.replace("?", "?\n")
+    text = text.replace(":", ":\n")
+    text = text.rstrip()
+
+    if len(text) == 0:
+        return
+
+    for key, line in enumerate(list(text.split("\n"))):
+        line = line.strip()
+        print("{}".format(line))
+        if key < len(list(text.split("\n"))) - 1:
+            print()
