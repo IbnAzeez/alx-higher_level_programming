f8f54c76c1ab28ca87e82f3dee9ed6add51221cc
IbnAzeez <hayzed003@gmail.com>
2023-03-16 03:59:56 -0700

    Add

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
 
-/**
- * Add two numbers
- *
- * @param {*} a first number
- * @param {*} b second number
- *
- * @returns result of addition of two parameters
- */
- exports.add = function (a, b) {
-  return (a + b);
-}
+exports.add = function (a, b) {
+  return a + b;
+};
